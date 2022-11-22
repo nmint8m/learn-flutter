b74125902ce59c53af1b67e24e7e53548e124774
Tam Nguyen Minh <nmint8m@gmail.com>
2022-11-22 10:55:02 +0700

    Generate folder

---
+++ a_macos_dev_env_first_app/android/app/src/main/kotlin/com/example/a_macos_dev_env_first_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.a_macos_dev_env_first_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ a_quiz_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(AQuizApp());
+}
+
+class AQuizApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
+}
