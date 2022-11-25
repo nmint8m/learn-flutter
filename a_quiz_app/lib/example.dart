ffea54d360a902a31a9d1fcfbd39a314c541c009
Tam Nguyen Minh <nmint8m@gmail.com>
2022-11-25 10:02:52 +0700

    First summary and additional syntax

---
+++ a_quiz_app/lib/example.dart
@@ -0,0 +1,26 @@
+class Person {
+  String name;
+  int age;
+
+  Person({String name, int age}) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Pet {
+  String name;
+  int age;
+
+  Pet({String name = 'A', int age = 0}) {
+    this.name = name;
+    this.age = age;
+  }
+}
+
+class Shoe {
+  String brand;
+  int size;
+
+  Shoe({this.brand, this.size});
+}
--- a_quiz_app/lib/main.dart
+++ a_quiz_app/lib/main.dart
@@ -1,16 +1,25 @@
 import 'package:flutter/material.dart';
-
+import 'example.dart';
 
 void main() {
   runApp(AQuizApp());
+
+  var o1 = Person(age: 40, name: "Tam");
+  print(o1.name);
+  var o2 = Pet();
+  print(o2.name);
+  var o3 = Shoe(brand: 'UA', size: 38);
+  print(o3.brand);
 }
 
 class AQuizApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text('Hello'),
+      home: Scaffold(
+        appBar: AppBar(title: Text('A Quiz App')),
+        body: Text('Lorem ipsum'),
+      ),
     );
   }
 }
