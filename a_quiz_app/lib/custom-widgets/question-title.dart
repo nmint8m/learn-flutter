505229a7481f89ca0df1dc48d280514c93f57aa5
Tam Nguyen Minh <nmint8m@gmail.com>
2022-12-06 10:34:41 +0700

    Styling and layouting step

--- a_quiz_app/lib/custom-widgets/question-title.dart
+++ a_quiz_app/lib/custom-widgets/question-title.dart
@@ -5,13 +5,20 @@ import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
 
 class QuestionTitle extends StatelessWidget {
-
   final String questionString;
 
   QuestionTitle(this.questionString);
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionString);
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(5),
+      child: Text(
+        questionString,
+        style: TextStyle(fontSize: 32),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
